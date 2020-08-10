c19d798787e3f8448aa917f91a3285d453e8f169
rashedhasan007 <49535604+rashedhasan007@users.noreply.github.com>
2020-08-10 05:42:13 -0700

    Update r.py

--- r.py
+++ r.py
@@ -5,86 +5,54 @@ warnings.warn = warn
 import time
 import logging
 import absl.logging
+import investpy
 logging.root.removeHandler(absl.logging._absl_handler)
 absl.logging._warn_preinit_stderr = False
 logging.basicConfig(filename='log.txt', filemode='w', datefmt='%Y-%m-%d %H:%M:%S', level=logging.INFO, format='%(asctime)-15s %(message)s')
 logging.info('logs to file, as expected')
 from iqoptionapi.stable_api import IQ_Option
-import candle1
+
 API = IQ_Option("rashedhasanai@gmail.com","rostugbot007")
 API.connect()
 API.change_balance('PRACTICE') # PRACTICE / REAL
 while True:
     if API.check_connect() == False:
-        print('Erro ao se conectar')
-        API.connect()
+    	print('Erro ao se conectar')
+    	API.connect()
     else:
     	print('Conectado com sucesso')
     	break
 
-res1=['win']
-money=[70,100,230,550,1275,3000]
-def multiply(res1,money):
-    if res1[len(res1)-1]=='win':
-        return 70
-    elif res1[len(res1)-1]=='loose':
-        i=len(res1)-1
-        count=0
-        while True:
-            if res1[i]=='win':
-                break
-            elif res1[i]=='loose':
-                count=count+1
-            i=i-1
-        return money[count]
 
-
-def check(a):
-    M=multiply(res1,money)
-    status,id = API.buy(M,"EURUSD",a,5)
-    p=API.check_win_v3(id)
-    print(p)
-    if p>0:
-      res1.append('win')
-    elif p<0:
-      res1.append('loose')
-    else:
-      None
-    return p
-
-def res_len(res):
-    if len(res)==7:
-        res.remove(res[1])
-    else:
-        None
-    
-    
-def my_function():
-    i1=API.get_candles("EURUSD",300,1,time.time())[0]['id']
-    while True:
-        end_from_time=time.time()
-        dat=API.get_candles("EURUSD",300,1,time.time())[0]
-        i=dat['id']
-        if i>i1:
-            print("start")
-            break
-    print("ok")
-    while True:
-        end_from_time=time.time()
-        data=API.get_candles("EURUSD",300,5, end_from_time)
-        l=data
-        res_len(res1)
-        try:
-            t=candle1.candle_score(l[3],l[2],l[1])[0]
-        except:
-            None
-        if t>0:
-            v=check("call")
-            print(candle1.candle_score(l[3],l[2],l[1]),v)
-        elif t<0:
-            v1=check("put")
-            print(candle1.candle_score(l[3],l[2],l[1]),v1)
-        else :
-            None
-        time.sleep(3)
-my_function()
+#fore=s['forecast'][5]
+fore='100k'
+#act=s['previous'][5]
+act='500k'
+def buyoption(forecast,actual):
+  if forecast[0]>=actual[0]:
+    status,id = API.buy(200,"EURUSD",'put',1)
+  elif forecast[0]<=actual[0]:
+    status,id = API.buy(200,"EURUSD",'call',1)
+  else:
+    None
+s=investpy.get_calendar('GMT +6:00','time_only',None,['high'],None,None,None)
+try:
+    print(s['currency'],s['previous'],s['actual'],s['forecast'])
+except:
+    None
+i=int(input())
+while True:
+  s=investpy.get_calendar('GMT +6:00','time_only',None,['high'],None,None,None)
+  fore=s['forecast'][i]
+  act=s['actual'][i]
+  if fore!=None and act!=None:
+    newstr = ''.join((ch if ch in '0123456789.-e' else ' ') for ch in fore)
+    forecast= [float(i) for i in newstr.split()]
+    print(forecast)
+    newstr = ''.join((ch if ch in '0123456789.-e' else ' ') for ch in act)
+    actual= [float(i) for i in newstr.split()]
+    print(actual)
+    buyoption(forecast,actual)
+    break
+  else:
+    None
