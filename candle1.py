01a05306094a82eced86c0bc53dc590f66ae86df
rashedhasan007 <49535604+rashedhasan007@users.noreply.github.com>
2020-10-21 06:22:07 -0700

    Update candle1.py

--- candle1.py
+++ candle1.py
@@ -6,19 +6,11 @@ def candle_score(lst_0,lst_1,lst_2):
     O_2,H_2,L_2,C_2=lst_2['open'],lst_2['max'],lst_2['min'],lst_2['close']
     
 
-    Bearish_Engulfing=((C_1 > O_1) & (O_0 > C_0)) & ((O_0 >= C_1) & (O_1 >= C_0)) & ((O_0 - C_0) > (C_1 - O_1 ))
-    
-    Bullish_Engulfing=(O_1 > C_1) & (C_0 > O_0) & (C_0 >= O_1) & (C_1 >= O_0) & ((C_0 - O_0) > (O_1 - C_1 ))
     
     
 
     strCandle=''
     candle_score=0
     
-    if    Bearish_Engulfing:
-        strCandle=strCandle+'/ '+'Bearish_Engulfing'
-        candle_score=candle_score-1
-    if    Bullish_Engulfing:
-        strCandle=strCandle+'/ '+'Bullish_Engulfing'
-        candle_score=candle_score+1
+    
     return candle_score,strCandle
