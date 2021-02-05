6d40ea415ec71a2ece30b3c191d6c40ba03bd6c8
rashedhasan007 <49535604+rashedhasan007@users.noreply.github.com>
2021-02-05 15:45:51 +0600

    Update r_final.py

--- r_final.py
+++ r_final.py
@@ -282,7 +282,7 @@ schedule.every().sunday.at("15:30").do(rani_take_lov)
 schedule.every().monday.at("13:15").do(rani_take_lov)
 schedule.every().wednesday.at("07:50").do(rani_take_lov)
 schedule.every().thursday.at("08:32").do(rani_take_lov)
-schedule.every().friday.at("08:58").do(rani_take_lov)
+schedule.every().friday.at("09:50").do(rani_take_lov)
 while True:
     schedule.run_pending()
     time.sleep(1)
